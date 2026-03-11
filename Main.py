e8f036317da766db4bd3e0d3116848e32d2a7ea0
Brendan Ng <brendanng88@gmail.com>
2026-03-10 23:18:49 -0400

    finished

---
+++ Main.py
@@ -0,0 +1,40 @@
+import random
+from gtts import gTTS
+import os
+import platform
+
+# -------------------------
+# List of your Spanish questions
+questions = [
+    "¿Has hecho ejercicio con tu amigo?",
+    "¿Has tenido un entrenador o un nutricionista?",
+    "¿Has corrido en una cinta caminadora?",
+    "¿Te has entrenado para un deporte?",
+    "¿Has levantado pesas esta semana?",
+    "¿Te habías apurado para llegar a esta clase a tiempo?",
+    "¿Habías roto un hueso antes de venir a la escuela secundaria?",
+    "¿Habías comido una merienda antes de llegar a esta clase?",
+    "¿Te habías mantenido en forma durante el invierno?",
+    "¿Habías estudiado antes de tomar este examen?"
+]
+
+
+question = random.choice(questions)
+print(question)
+
+tts = gTTS(text=question, lang='es', slow=False)  
+
+
+audio_file = "question.mp3"
+tts.save(audio_file)
+
+# Play audio depending on OS
+if platform.system() == "Windows":
+    os.system(f"start {audio_file}")
+elif platform.system() == "Darwin":  
+    os.system(f"afplay {audio_file}")
+else:  
+    os.system(f"mpg123 {audio_file}")  
+
+
+
